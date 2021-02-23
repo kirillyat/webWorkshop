41014bc8ee3c507edb20b35809776a051ea3e097
kirillyat <kirillyat@gmail.com>
2021-02-23 11:40:19 +0300

    Update create.sql

--- database/create.sql
+++ database/create.sql
@@ -1,25 +1,21 @@
 CREATE TABLE people(
-	id UInt64, -- Индефикатор
-	name String, -- ФИО
-
-	mother_id UInt64, -- ID Матери
-	father_id UInt64, -- ID Отца
-	birth_date Date, -- Дата рождения
-
-	description String, -- Описание
-	death_date Date NULL, -- Дата смерти | NULL
-	-- TODO: Места проживания
-
+	id UInt64, 				-- Индефикатор
+	name String, 			-- ФИО
+	mother_id UInt64, 		-- ID Матери
+	father_id UInt64, 		-- ID Отца
+	birth_date Date, 		-- Дата рождения
+	description String,		-- Описание
+	death_date Date NULL,	-- Дата смерти | NULL
+							-- TODO: Места проживания
 	PRIMARY KEY(id)
 );
 
 
 CREATE TABLE marriage(
-	wife_id UInt64, -- ID Супрги
-	husband_id UInt64, -- ID Супруга
-
-	marriage_date Date, -- Дата заключения брака
-	divorce_date Date NULL -- Дата расторжения брака | NULL
+	wife_id UInt64, 		-- ID Супрги
+	husband_id UInt64, 		-- ID Супруга
+	marriage_date Date, 	-- Дата заключения брака
+	divorce_date Date NULL 	-- Дата расторжения брака | NULL
 );
 
 
