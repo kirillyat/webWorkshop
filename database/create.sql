f3c28f072f3703f6dc02204b7ea8acfda22c5b60
kirillyat <kirillyat@gmail.com>
2021-02-24 23:02:54 +0300

    Final Part #1

--- database/create.sql
+++ database/create.sql
@@ -1,15 +1,22 @@
+DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS marriage;
+
+DROP DATABASE IF EXISTS FamilyTree;
+CREATE DATABASE FamilyTree;
+\connect FamilyTree
+
 CREATE TABLE people (
     id UInt64,              -- Индефикатор
     name String,            -- ФИО
     mother_id UInt64        -- ID Матери
-    REFERENCES people(id),       
+    REFERENCES people(id),
     father_id UInt64        -- ID Отца
     REFERENCES people(id),
     birth_date Date,        -- Дата рождения
     death_date Date NULL,   -- Дата смерти | NULL
     description String,     -- Описание
     address String,         -- Места проживания
-    
+
     PRIMARY KEY(id)
 );
 
@@ -20,7 +27,7 @@ CREATE TABLE marriage (
     marriage_date Date,     -- Дата заключения брака
     divorce_date Date NULL, -- Дата расторжения брака | NULL
 
-    FOREIGN KEY(wife_id) 
+    FOREIGN KEY(wife_id)
     REFERENCES people(id),
 
     FOREIGN KEY(husband_id)
---
+++ database/fill.sql
@@ -0,0 +1,2 @@
+COPY people FROM 'people.csv' DELIMITER '|';
+COPY merrige FROM 'merrige.csv' DELIMITER '|';
