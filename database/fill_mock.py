4a076d6dabc1972a996929005abf7e4147471bd8
kirillyat <kirillyat@gmail.com>
2021-02-24 22:48:58 +0300

    MOCK DATA

--- database/fill_mock.py
+++ database/fill_mock.py
@@ -1,25 +1,63 @@
-
-import random
-from mimesis import Person, Generic, locales
+from random import randint
+from mimesis import Person, Generic, locales, Address
 from mimesis.enums import Gender
 from mimesis.builtins import RussiaSpecProvider
 
+
+p = open("people.csv",  'w')
+m = open("merrige.csv", 'w')
+
+#Objects for mimesis
 person = Person(locales.RU)
 ru = RussiaSpecProvider()
 g = Generic('ru')
+a = Address('ru')
 
-def makeMan():
-    print(person.full_name(gender=Gender.MALE)+" "+
-          ru.patronymic(gender=Gender.MALE))
-    print(g.datetime.date())
-    print(ru.generate_sentence())
+def makeManName():
+    return str(person.full_name(gender=Gender.MALE)
+            + " " +
+            ru.patronymic(gender=Gender.MALE))
 
-def makeWoman():
-    print(person.full_name(gender=Gender.FEMALE)+ " " +ru.patronymic(gender=Gender.FEMALE))
-    print(g.datetime.date())
-
-def marrige():
-    print("hello")
+def makeWomanName():
+    return  str(person.full_name(gender=Gender.FEMALE)
+            + " " +
+            ru.patronymic(gender=Gender.FEMALE))
 
 
-makeMan()
+'''
+id UInt64,              -- Индефикатор
+name String,            -- ФИО
+mother_id UInt64,       -- ID Матери
+father_id UInt64,       -- ID Отца
+birth_date Date,        -- Дата рождения
+death_date Date NULL,   -- Дата смерти | NULL
+description String,     -- Описание
+address String,         -- Места проживания
+'''
+def makePopulation(f, n):
+    for i in range(0,n,2):
+        S = str(i) + '|' + makeManName() + '|'
+        S+= str(randint(0,i)) + '|'+ str(randint(0,i)) + '|'
+        S+= str(g.datetime.date()) + '|' + '|'
+        S+= ru.generate_sentence() + '|'
+        S+= a.address()+'\n'
+        f.write(S)
+        S = str(i+1) + '|' + makeWomanName() + '|'
+        S+= str(randint(0,i)) + '|'+ str(randint(0,i)) + '|'
+        S+= str(g.datetime.date()) + '|' + '|'
+        S+= ru.generate_sentence() + '|'
+        S+= a.address()+'\n'
+        f.write(S)
+
+def makeMarrige(f, n):
+    for i in range(n):
+        S= str(randint(0,n)) + '|'+ str(randint(0,n)) + '|'
+        S+= str(g.datetime.date()) + '|' + str(g.datetime.date()) +'\n'
+        f.write(S)
+
+n = 100
+makePopulation(p, n)
+makeMarrige(m, n)
+
+p.close()
+m.close()
