12d47b1d50d64a97e9037860ea79b854e4df990b
kirillyat <kirillyat@gmail.com>
2021-02-24 19:55:44 +0300

    Update create.sql

--- database/create.sql
+++ database/create.sql
@@ -1,26 +1,26 @@
 CREATE TABLE people (
-	id UInt64, 				-- Индефикатор
-	name String, 			-- ФИО
-	mother_id UInt64, 		-- ID Матери
-	father_id UInt64 , 		-- ID Отца
-	birth_date Date, 		-- Дата рождения
-	death_date Date NULL,	-- Дата смерти | NULL
-	description String,		-- Описание
-	address String,			-- Места проживания
+    id UInt64,              -- Индефикатор
+    name String,            -- ФИО
+    mother_id UInt64,       -- ID Матери
+    father_id UInt64 ,      -- ID Отца
+    birth_date Date,        -- Дата рождения
+    death_date Date NULL,   -- Дата смерти | NULL
+    description String,     -- Описание
+    address String,         -- Места проживания
     
-	PRIMARY KEY(id)
+    PRIMARY KEY(id)
 );
 
 
 CREATE TABLE marriage (
-	wife_id UInt64, 		-- ID Супрги
-	husband_id UInt64, 		-- ID Супруга
-	marriage_date Date, 	-- Дата заключения брака
-	divorce_date Date NULL, -- Дата расторжения брака | NULL
+    wife_id UInt64,         -- ID Супрги
+    husband_id UInt64,      -- ID Супруга
+    marriage_date Date,     -- Дата заключения брака
+    divorce_date Date NULL, -- Дата расторжения брака | NULL
 
-	FOREIGN KEY(wife_id) 
-	REFERENCES people(id),
+    FOREIGN KEY(wife_id) 
+    REFERENCES people(id),
 
-	FOREIGN KEY(husband_id)
-	REFERENCES people(id),
+    FOREIGN KEY(husband_id)
+    REFERENCES people(id),
 );
