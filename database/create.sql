b940bc94c99b8338ebe8e230a535fa6f9e83c697
kirillyat <kirillyat@gmail.com>
2021-02-24 20:25:13 +0300

    +

--- database/create.sql
+++ database/create.sql
@@ -1,8 +1,10 @@
 CREATE TABLE people (
     id UInt64,              -- Индефикатор
     name String,            -- ФИО
-    mother_id UInt64,       -- ID Матери
-    father_id UInt64 ,      -- ID Отца
+    mother_id UInt64        -- ID Матери
+    REFERENCES people(id),       
+    father_id UInt64        -- ID Отца
+    REFERENCES people(id),
     birth_date Date,        -- Дата рождения
     death_date Date NULL,   -- Дата смерти | NULL
     description String,     -- Описание
